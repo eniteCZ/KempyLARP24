dfbf5a79bd7471953c031047ac5016cc353d1e80
eniteCZ <f.radmanovic@gmail.com>
2024-07-11 23:01:12 +0200

    Druhá mise

--- scenario_101_hra_na_schovavanou.lua
+++ scenario_101_hra_na_schovavanou.lua
@@ -1,4 +1,4 @@
--- Name: Prelet Na Karmelis
+-- Name: Hra Na Schovávanou
 -- Description: Přelet na planetu Karmelis
 ---
 --- Scenario
@@ -7,6 +7,9 @@
 require("utils.lua")
 KARMELIS_X = -17289
 KARMELIS_Y = 8764
+
+EPIMETEUS_X = 265834
+EPIMETEUS_Y = 117500
 
 function init()
     karmelis = Planet():setPosition(KARMELIS_X, KARMELIS_Y):setPlanetRadius(2500):setDistanceFromMovementPlane(-2000.00)
@@ -19,7 +22,7 @@ function init()
         karmelisMoon:setPlanetSurfaceTexture("planets/moon-2.png"):setAxialRotationTime(220.00)
         karmelisMoon:setCallSign("Nero")
     
-    local epimeteus = Planet():setPosition(265834, 117500):setPlanetRadius(5000):setPlanetCloudRadius(5200.00):setDistanceFromMovementPlane(-2000)
+    local epimeteus = Planet():setPosition(EPIMETEUS_X, EPIMETEUS_Y):setPlanetRadius(5000):setPlanetCloudRadius(5200.00):setDistanceFromMovementPlane(-2000)
         epimeteus:setPlanetSurfaceTexture("planets/planet-1.png"):setAxialRotationTime(120.00)
         epimeteus:setPlanetAtmosphereColor(0.361, 0.263, 0.541):setPlanetAtmosphereTexture("planets/atmosphere.png")
         epimeteus:setPlanetCloudTexture("planets/clouds-3.png")
@@ -33,7 +36,7 @@ function init()
         sun1:setCallSign("Pela")
 
     -- Spawn the Ark
-    Archa = PlayerSpaceship():setTemplate("Ender"):setPosition(-17224, -14649):setFaction("Citadel")
+    Archa = PlayerSpaceship():setTemplate("Ender"):setPosition(275834, 118500):setFaction("Citadel")
         Archa:setCallSign('Archa Noe-02')
         -- Blbuvzdornost
         Archa:setCanSelfDestruct(false)
@@ -47,7 +50,7 @@ function init()
         Archa:setHullMax(1000):setHull(714)
         Archa:setSystemHealthMax("reactor", 0.6):setSystemHealth("reactor", 0.4)
         Archa:setSystemHealthMax("maneuver", 0.65):setSystemHealth("maneuver", 0.3)
-        Archa:setSystemHealthMax("impulse", 0.2):setSystemHealth("impulse", 0.1)
+        Archa:setSystemHealthMax("impulse", 0.8):setSystemHealth("impulse", 0.5)
         Archa:setSystemHealthMax("warp", 0.0):setSystemHealth("warp", 0.0)
         Archa:setSystemHealthMax("jumpdrive", 0.0):setSystemHealth("jumpdrive", 0.0)
         Archa:setSystemHealthMax("beamweapons", 0.8):setSystemHealth("beamweapons", 0.4)
@@ -58,26 +61,8 @@ function init()
         Archa:setJumpDrive(false):setWarpDrive(false):setImpulseMaxSpeed(250)
         Archa:setSystemPowerFactor('reactor', -52)
 
-    CpuShip():setFaction("Vorcha"):setTemplate("Blade"):setCallSign("NC3"):setPosition(-63886, -3756):orderRoaming()
-    CpuShip():setFaction("Vorcha"):setTemplate("Blade"):setCallSign("VS4"):setPosition(-66740, -11828):orderRoaming()
-    CpuShip():setFaction("Vorcha"):setTemplate("Blade"):setCallSign("BR2"):setPosition(-64946, -7833):orderRoaming()
-   
-    CpuShip():setFaction("Vorcha"):setTemplate("Personnel Freighter 1"):setCallSign("VK7"):setPosition(-58121, -7130):orderAttack(Archa):setBeamWeapon(0, 48, 0, 500, 20.0, 0.2):setBeamWeaponTurret(0, 0, 0, 0):setImpulseMaxSpeed(129.2):setRotationMaxSpeed(11.4):orderFlyFormation(Archa, 300, 150)
-    CpuShip():setFaction("Vorcha"):setTemplate("Personnel Freighter 1"):setCallSign("NC6"):setPosition(-60049, -10799):orderAttack(Archa):setBeamWeapon(0, 48, 0, 500, 20.0, 0.2):setBeamWeaponTurret(0, 0, 0, 0):setImpulseMaxSpeed(129.2):setRotationMaxSpeed(11.4)
-    CpuShip():setFaction("Vorcha"):setTemplate("Personnel Freighter 1"):setCallSign("CSS5"):setPosition(-61354, -14653):orderAttack(Archa):setBeamWeapon(0, 48, 0, 500, 20.0, 0.2):setBeamWeaponTurret(0, 0, 0, 0):setImpulseMaxSpeed(129.2):setRotationMaxSpeed(11.4)
-    CpuShip():setFaction("Vorcha"):setTemplate("Personnel Freighter 1"):setCallSign("S8"):setPosition(-66266, -15959):orderAttack(Archa):setBeamWeapon(0, 48, 0, 500, 20.0, 0.2):setBeamWeaponTurret(0, 0, 0, 0):setImpulseMaxSpeed(129.2):setRotationMaxSpeed(11.4)
-
-  
-    -- Send message to the communications officer
-    Archa:addCustomMessage("Relay", 'Hlášení' ,"VITA: Archa Noe 02 se nachází na okraji galaxie Mléčná Dráha v soustavě Galathea. Nejbližší Mass Rellay je v Armstrongově mlhovině. Kvůli nedostatku Eeza a poškozené soustavě impulzního pohonu není Archa schopna do mlhoviny doletět. V soustavě se nachází dvě obydlené planety: Karmelis a Epimeteus. Archa se nachází v relativní blízkosti planety Karmelis. Záznamy v databázi ukazují, že by se zde měla nacházet výzkumná stanice. Tato stanice by mohla disponovat zásobami které by mohly pomoci v naší situaci. Přiletět ke Karmelis a navázat spojení.")
-    Archa:addCustomMessage("Engineering", 'Hlášení poškození', 'Zásoby Eeza: Kritické. Nadsvětelný pohon: Vyřazený kvůli nedostatku Eeza. Impulzní pohon: Kritický - Regulátor plazmového proudění nefunkční. Reaktor: Režim úspory. Manévrovací motory: Počkozené výbuchem Eeza.' )
-    Archa:addCustomMessage("Weapons", 'Výstraha', 'Okrajové planety jsou známé pirátskými útoky. Doporučuji ostražitost.')
-    Archa:addCustomMessage("Science", 'Doporučení', 'Při příletu k planetě Karmelis doporučuji provést průzkum atmosféry a povrchu.')
-
-
 end
 
-addGMFunction("Players Win", function() victory("Citadel") end)
 
 function update(delta)
     -- Check if the player's ship is within 3 units of Karmelis
@@ -96,24 +81,3 @@ onNewPlayerShip(
         -- ship:destroy()
     end
 )
-
-
-
-Nebula():setPosition(-70296, 50580)
-Nebula():setPosition(-75605, 44863)
-Nebula():setPosition(-82412, 41188)
-Nebula():setPosition(-85951, 30298)
-Nebula():setPosition(-85542, 18727)
-Nebula():setPosition(-87857, 10423)
-Nebula():setPosition(-86495, 1984)
-Nebula():setPosition(-88946, -5503)
-Nebula():setPosition(-90307, 21177)
-Nebula():setPosition(-91532, 36015)
-Nebula():setPosition(-81323, 8245)
-Nebula():setPosition(-74925, 486)
-Nebula():setPosition(-80642, -4550)
-Nebula():setPosition(-74516, -10540)
-Nebula():setPosition(-70977, -5776)
-Nebula():setPosition(-94663, 27439)
-
-
